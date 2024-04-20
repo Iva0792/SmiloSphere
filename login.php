c77fb353ef16e6c77292f3dc7b969b3df1873ee0
Ivania Matamoros <imabe92@gmail.com>
2024-04-19 23:11:00 -0600

    modified:   login.php

--- login.php
+++ login.php
@@ -1,50 +1,49 @@
 <?php
-                /*if (array_key_exists('login-form-submit', $_POST)) {
-                    submit();
-                }
+/*if (array_key_exists('login-form-submit', $_POST)) {
+    submit();
+}
 
-                function submit()
-                {*/
-                    session_start();
+function submit()
+{*/
+session_start();
 
-                    $sname = "localhost";
-                    $dname = "u884001650_smiloadmin";
-                    $password = "4&Mk/>OcxOsX";
-                    $db_name = "u884001650_SmiloSphereDB";
+$sname = "localhost";
+$dname = "u884001650_smiloadmin";
+$password = "4&Mk/>OcxOsX";
+$db_name = "u884001650_SmiloSphereDB";
 
-                    $conn = mysqli_connect($sname, $dname, $password, $db_name);
+$conn = mysqli_connect($sname, $dname, $password, $db_name);
 
-                    if (!$conn) {
+if (!$conn) {
 
-                        echo "Connection failed!";
+    echo "Connection failed!";
 
-                    } else {
+} else {
 
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            $username = $_POST["username"];
-                            $password = $_POST["password"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-                            $stmt = $conn->prepare("CALL userLogin(?, ?, @responseMessage)");
-                            $stmt->bind_param("ss", $username, $password);
+        $stmt = $conn->prepare("CALL userLogin(?, ?, @responseMessage)");
+        $stmt->bind_param("ss", $username, $password);
 
-                            $stmt->execute();
-                            $stmt->close();
+        $stmt->execute();
+        $stmt->close();
 
-                            $result = $conn->query("SELECT @responseMessage AS responseMessage");
-                            $row = $result->fetch_assoc();
-                            $responseMessage = $row["responseMessage"];
+        $result = $conn->query("SELECT @responseMessage AS responseMessage");
+        $row = $result->fetch_assoc();
+        $responseMessage = $row["responseMessage"];
 
-                            if ($responseMessage === 'User successfully logged in') {
-                                session_start();
-                                $_SESSION['username'] = $username;
-                                // Redirect to home page
-                                header('Location: home.php');
-                                exit;
-                            } else {
-                                echo 'Error al iniciar sesión';
-                            }
+        if ($responseMessage === 'User successfully logged in') {
+            session_start();
+            $_SESSION['username'] = $username;
+            // Redirect to home page
+            header('Location: home.php');
+            exit;
+        } else {
+            echo 'Error al iniciar sesión';
+        }
 
-                        }
-                        $conn->close();
-                    }
-                ?>
+    }
+    $conn->close();
+}
