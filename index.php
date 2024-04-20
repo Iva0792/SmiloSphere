bcca1343be8908da166aa1a621a7007d01e0435f
Ivania Matamoros <imabe92@gmail.com>
2024-04-20 17:18:55 -0600

    modified:   index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no" <link rel="stylesheet"
-        href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/util.css">
     <title>SmiloSphere</title>
 </head>
