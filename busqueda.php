d25b5f9867d8a921726b04dddd23902f07afd338
Ivania Matamoros <imabe92@gmail.com>
2024-04-21 16:33:27 -0600

    modified:   agenda.php 	modified:   busqueda.php 	modified:   expedientes.php 	modified:   facturacion.php 	modified:   mensajeria.php 	modified:   pendientes.php 	modified:   restore.php

--- busqueda.php
+++ busqueda.php
@@ -1,9 +1,10 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no" <link rel="stylesheet"
-        href="css/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/util.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <title>SmiloSphere | Búsqueda</title>
 </head>
 
--- restore.php
+++ restore.php
@@ -1,7 +1,8 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no" <link rel="stylesheet"
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+     <link rel="stylesheet"
         href="css/main.css">
     <link rel="stylesheet" href="css/util.css">
     <title>SmiloSphere | Recuperación</title>
