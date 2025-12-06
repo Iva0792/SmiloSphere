63168782d5c30a2bad3391aae2d808c157ec5596
Iva0792 <imabe92@gmail.com>
2025-12-05 21:29:41 -0600

    deleted:    dist/assets/Montserrat-VariableFont_wght-i02rBV5P.ttf 	deleted:    dist/assets/PlayfairDisplay-Regular-BzGjw5Te.ttf 	deleted:    dist/assets/bg-01-BN9wkkb7.svg 	modified:   dist/index.html 	deleted:    dist/vite.svg 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/)) return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-
-        return esbuild.transform(code, { loader: 'js' });
-      },
-    },
     react(),
   ],
-
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 
   base: '/',
   build: {
     outDir: 'dist',
-  },
-
+  }
 })
